bae9db3cdd720459afc0951c336d67e6b283aaa9
David Vanhoucke <dvanhoucke@redborder.com>
2024-11-15 09:19:45 +0000

    add possibility to add user repo url

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-# user redborder-repo
+# danhoucke-repo
 
-generate repo for specific user. User can be overwritten with USERNAME
-example : USERNAME=dvanhoucke make rpm
+Build a particular product version:
+
+```
+PRODUCT_VERSION=23.11 VERSION=1.0.0 make rpm
+```
+
+Build latest (by default PRODUCT_VERSION=latest):
+
+```
+VERSION=1.0.0 make rpm
+```
+
+Build with different URL:
+```
+VERSION=1.0.0 REPO_URL="http://packages.redborder.com" make rpm
+```
+
+Build with different USER URL:
+```
+VERSION=1.0.0 USER REPO_URL="http://packages-sc.redborder.lan" make rpm
+```
--- packaging/rpm/placeholders.sh
+++ packaging/rpm/placeholders.sh
@@ -4,7 +4,7 @@
 files=("resources/username.repo")
 
 f_usage() {
-  echo "usage: $0 -u USERNAME -v VERSION -b BUILDNUMBER"
+  echo "usage: $0 -u USERNAME -v VERSION -b BUILDNUMBER -r REPO_URL -l USER_REPO_URL"
   exit 0
 }
 
@@ -16,25 +16,23 @@ while getopts "hu:v:b:" opt; do
           ;;
       b) buildnumber="$OPTARG"
           ;;
+      r) repo_url="$OPTARG"
+          ;;
+      l) user_repo_url="$OPTARG"
+          ;;
       *) f_usage
           ;;
   esac
 done
 
-if [ -z $username ]; then
-    f_usage
-fi
-
-if [ -z $version ]; then
-    f_usage
-fi
-
-if [ -z $buildnumber ]; then
+if [ -z "$username" ] || [ -z "$version" ] || [ -z "$buildnumber" ] || [ -z "$repo_url" ] || [ -z "$user_repo_url" ]; then
     f_usage
 fi
 
 for file in "${files[@]}"; do
     sed -i "s/{{USERNAME}}/$username/g" $file
     sed -i "s/{{VERSION}}/$version/g" $file
+    sed -i "s/{{REPO_URL}}/$repo_url/g" $file
+    sed -i "s/{{USER_REPO_URL}}/$user_repo_url/g" $file
     sed -i "s/{{BUILDNUMBER}}/$buildnumber/g" $file
 done
